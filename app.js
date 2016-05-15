4d3e451f9d3dfadde184cfbb8bba10701ea931b2
mishacreatrix <michellemcc76@gmail.com>
2016-05-15 21:46:07 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var express = require('express');
 
 var app = express(); 
 var mongoose = require('mongoose');
-//mongoose.connect('mongodb://localhost/users_db');
-mongoose.connect('mongodb://michelle:123456@ds023432.mlab.com:23432/smartgoaltracker');
+mongoose.connect('mongodb://localhost/users_db');
+//mongoose.connect('mongodb://michelle:123456@ds023432.mlab.com:23432/smartgoaltracker');
 
 require('./config/express').addMiddleware(app)
 require('./routes')(app)
